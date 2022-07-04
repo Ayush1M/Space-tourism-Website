dcf9153544551a521c42ba4827269d952a9c50d1
Ayush1M <worklife2442@gmail.com>
2022-07-04 23:22:51 +0530

    Refactoring and added functioning to crew page

--- tab.js
+++ tab.js
@@ -1,6 +1,7 @@
 const tabList = document.querySelector('[role="tablist"]')
 const tabs = tabList.querySelectorAll('[role= "tab"]')
 
+
 tabs.forEach((tab) =>{
     tab.addEventListener("click", changeTabPanel)
 })
@@ -8,11 +9,25 @@ tabs.forEach((tab) =>{
 function changeTabPanel(e){
     const targetTab = e.target
     const targetPanel = targetTab.getAttribute("aria-controls")
+    const targetImage = targetTab.getAttribute("data-image")
 
     const tabContainer = targetTab.parentNode;
     const mainContainer = tabContainer.parentNode;
-     
-    mainContainer.querySelectorAll('article').forEach((article) => article.setAttribute("hidden", true))
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    tabContainer.querySelector('[aria-selected="true"]').setAttribute("aria-selected", false)
+    targetTab.setAttribute("aria-selected", true)
+
+    hideContent(mainContainer, 'article')
+    showContent(mainContainer, [`#${targetPanel}`])
+     
+    hideContent(mainContainer, 'picture')
+    showContent(mainContainer, [`#${targetImage}`])
+}
+
+function hideContent(parent, content){
+    parent.querySelectorAll(content).forEach((item) => item.setAttribute("hidden", true))
+}
+
+function showContent(parent, content){
+    parent.querySelector(content).removeAttribute('hidden');
 }
